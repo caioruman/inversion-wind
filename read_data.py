f6e1cb1220fac9c2d9a11edad83986536d033dbc
Caio J Ruman <caioruman@gmail.com>
2020-05-18 16:40:54 -0400

    testing outputs

--- read_data.py
+++ read_data.py
@@ -13,10 +13,10 @@ import matplotlib as mpl # in python
 from matplotlib.colors import BoundaryNorm
 #from mpl_toolkits.basemap import Basemap
 #from mpl_toolkits.basemap import maskoceans
-#from netCDF4 import Dataset
+from netCDF4 import Dataset
 
-#from rpn.rpn import RPN
-#from rpn.domains.rotated_lat_lon import RotatedLatLon
+from rpn.rpn import RPN
+from rpn.domains.rotated_lat_lon import RotatedLatLon
 
 
 '''
@@ -39,8 +39,12 @@ for later:
 '''
 
 def main():
-    datai = 1981
+    datai = 1989
     dataf = 1990
+
+    # constants
+    Rd = 287  # Gas constant of dry air
+    g = 9.80665 # gravity
 
     # simulation
     exp = "cPanCan_011deg_675x540_SPN_ERA5_90lvl"
@@ -71,14 +75,48 @@ def main():
                 file_gem = "{0}/Samples/{1}_{2}{3:02d}/dm*".format(folder, exp, year, month)
 
                 file_list = glob(file_gem)
-                print(file_list)
                 file_list.sort()
 
-                print(file_list)
+                for f in file_list:
+
+                    r = RPN(f)
+                    
+                    gz = r.variables['GZ'][:]
+                    tt = r.variables['TT'][:]
+                    uu = r.variables['UU'][:]
+                    vv = r.variables['VV'][:]
+                    hu = r.variables['HU'][:] # specific humidity
+                    uv = np.sqrt(np.pow(uu,2) + np.pow(vv,2))
+
+                    print(gz[:,10,10])
+                    print(hu[:,10,10])
+                    print(uv[:,10,10])
+                    print(tt[:,10,10])
+
+                    sys.exit()
+
+                    mslp = r.variables['PN'][:]
+
+                    # Need to check the position of the 'surface level'
+
+                    # Virtual Temperature
+                    # Tv ~ T*(1 + 0.61*w)
+                    # Tv ~ T*(1 + 0.61*(hu/(1-hu)))
+                    tv = tt*(1 + 0.61*(hu/(1-hu)))
+
+                    # Using the hypsometric equation
+                    # Z2 - Z1 = (Rd*Tv)*ln(p1/p2)/g
+                    # at sea level, z1 = 0 and p1 = mslp
+                    # mslp/p2 = np.exp(Z2*g/(Rd*Tv))
+                    # p2 = mslp/np.exp(Z2*g/(Rd*Tv))
+                    p = mslp/(np.exp(gz*g/(Rd*tv)))
+
+                    
+
+                    r.close()
 
 
-        #arq_gem.close()
-        #arq_gz.close()
+
 
 
 def getMonths(period):
